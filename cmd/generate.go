99f674793030286aeffd5f03410d9ce50d0f2b10
Jon Barber <jon.barber@acm.org>
2019-05-06 17:01:26 +0100

    Hard coded grayscale transform reqs and always saves

--- cmd/generate.go
+++ cmd/generate.go
@@ -1,30 +1,74 @@
 package cmd
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/Pallinder/go-randomdata"
 	"github.com/hashicorp/go-retryablehttp"
 	"github.com/spf13/cobra"
+	"github.com/tsenart/vegeta/lib"
+	"net/http"
 	"os"
 )
 
-var url string
+var fetchUrl, transformUrl string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "o11y-transform",
+	Use:   "generator",
 	Short: "o11y workshop image transformation requests generator",
 	Long:  "generate a stream of image transform request data for vegeta. Uses the REST API to find image names prefixed with `gen_up_` and generates random transforms for them",
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		_, err := findImages()
+		images, err := findImages()
 
 		if err != nil {
 			return fmt.Errorf("unable to find images: %v\n", err)
 		}
 
+		for {
+			transformations := transform(images)
+			asTargets(transformations)
+		}
+
 		return nil
 	},
+}
+
+func transform(images []Image) []ImageTransformation {
+
+	var transforms []ImageTransformation
+
+	for _, image := range images {
+
+		transform := ImageTransformation{
+			ID:   image.ID,
+			Save: true,
+			Name: fmt.Sprintf("gen_tr_%s", randomdata.StringNumberExt(2, "-", 9)),
+			Transformations: []Transformation{
+				{Type: "grayscale"},
+			},
+		}
+
+		transforms = append(transforms, transform)
+
+	}
+
+	return transforms
+
+}
+
+type Transformation struct {
+	Type       string            `json:"type"`
+	Properties map[string]string `json:"properties"`
+}
+
+type ImageTransformation struct {
+	ID              string           `json:"imageId"`
+	Save            bool             `json:"persist"`
+	Name            string           `json:"name"`
+	Transformations []Transformation `json:"transformations"`
 }
 
 type Image struct {
@@ -34,7 +78,7 @@ type Image struct {
 }
 
 func findImages() ([]Image, error) {
-	resp, err := retryablehttp.Get(fmt.Sprintf("%s/nameContaining/gen_up_", url))
+	resp, err := retryablehttp.Get(fetchUrl)
 
 	if err != nil {
 		return nil, fmt.Errorf("unable to find uploaded images: %v", err)
@@ -51,12 +95,57 @@ func findImages() ([]Image, error) {
 
 }
 
+func asTargets(transformations []ImageTransformation) {
+
+	var buf bytes.Buffer
+	enc := vegeta.NewJSONTargetEncoder(&buf)
+
+	for _, transformation := range transformations {
+
+		target, err := asTarget(transformation)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "unable to process %q : %v", transformation, err)
+			break
+		}
+
+		err = enc.Encode(target)
+
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "unable to encode %q : %v", transformation, err)
+			break
+		}
+		_, _ = fmt.Fprintf(os.Stdout, "%s\n", string(buf.Bytes()))
+		buf.Reset()
+
+	}
+
+}
+
+func asTarget(transformation ImageTransformation) (*vegeta.Target, error) {
+
+	body, err := json.Marshal(transformation)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal trasformation request: %v", err)
+	}
+
+	target := vegeta.Target{
+		Method: "POST",
+		URL:    transformUrl,
+		Header: http.Header{"Content-Type": []string{"application/json "}},
+		Body:   body,
+	}
+
+	return &target, nil
+
+}
+
 func Execute() {
 
-	rootCmd.Flags().StringVarP(&url, "url", "u", "http://localhost:8081/api/images", "URL prefix for image API")
+	rootCmd.Flags().StringVarP(&fetchUrl, "fetchurl", "f", "http://localhost:8081/api/images/nameContaining/gen_up_", "API endpoint for finding images")
+	rootCmd.Flags().StringVarP(&transformUrl, "transformurl", "t", "http://localhost:8080/api/images/transform", "URL for image transformation")
 
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module generator
 go 1.12
 
 require (
+	github.com/Pallinder/go-randomdata v1.1.0
 	github.com/hashicorp/go-retryablehttp v0.5.3
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/influxdata/tdigest v0.0.0-20181121200506-bf2b5ad3c0a9 // indirect
+	github.com/mailru/easyjson v0.0.0-20190403194419-1ea4449da983 // indirect
 	github.com/spf13/cobra v0.0.3
 	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/tsenart/vegeta v12.1.0+incompatible
+	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c // indirect
+	golang.org/x/text v0.3.2 // indirect
+
 )
